80fd45f3a0358c7f7f3f32fd32e1a4e25816c21a
Francisco Biasuso <fbiasuso@gmail.com>
2025-01-14 18:56:04 -0300

    add dependencies and configure index.js

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express');
+//const express = require('express');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//import database from './database/database';
+
+//import {routerCustomers} from  './routes/customersRoutes';
+
 const app = express();
+
+
+app.set('PORT', process.env.PORT || 5000);
+
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+
+// app.use(routerCustomers);
 
 app.get('/', function(req, res){
     res.send('EXPRESS INSTALADO CORRECTAMENTE');
 });
 
-app.listen(3000, function () {
-    console.log('Escuchando el puerto 3000!');
+app.listen(app.get('PORT'), function () {
+    console.log(`Escuchando el puerto ${app.get('PORT')}!`);
 });
